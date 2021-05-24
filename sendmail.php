d568c981df01b48d0b0f825c99ebc2231bf26719
jem-cloud <84619193+jem-cloud@users.noreply.github.com>
2021-05-24 11:10:01 +0800

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,45 +1,51 @@
 <?php
 	use PHPMailer\PHPMailer\PHPMailer;
-	use PHPMailer\PHPMailer\Exception;
 
-	require 'path/to/PHPMailer/src/Exception.php';
-	require 'path/to/PHPMailer/src/PHPMailer.php';
-	require 'path/to/PHPMailer/src/SMTP.php';
+	require_once 'phpmailer/Exception.php';
+	require_once 'phpmailer/PHPMailer.php';
+	require_once 'phpmailer/SMTP.php';
+
+	$mail = new PHPMailer(true);
 
 	$alert = '';
-	
-	$mail = new PHPMailer();
-	$mail->IsSMTP();
-	$mail->Mailer = "smtp";
 
-	$mail->SMTPDebug  = 1;  
-	$mail->SMTPAuth   = TRUE;
-	$mail->SMTPSecure = "tls";
-	$mail->Port       = 587;
-	$mail->Host       = 'smtp.gmail.com';
-	$mail->Username   = 'jemina.cayme.sy@gmail.com';
-	$mail->Password   = 'ics2608asdfghjkl';
+	if(isset($_POST['submitForm'])){
+		$fname = $_POST['firstname'];
+		$lname = $_POST['lastname'];
+		$phoneNum = $_POST['phoneumber'];
+		$email = $_POST['email'];
+		$package = $_POST['country'];
+		$message = $_POST['subject'];
 
-	$mail->IsHTML(true);
-	$mail->AddAddress('jemina.cayme.sy');
-	$mail->SetFrom('jemina.cayme.sy');
- 
-	$mail->Subject = 'New Form Submission (Contact Us)';
-	$content = "<h3>First Name: $fname <br /> 
-			Last Name: $lname <br />
-			Phone Number: $phoneNum <br /> 
-			Email: $email <br /> 
-			Package: $package <br /> 
-			Message: $message</h3>";
+		try{
+			$mail->isSMTP();
+			$mail->Host = 'smtp.gmail.com';
+			$mail->SMTPAuth = true;
+			$mail->Username = 'jemina.cayme.sy@gmail.com';
+			$mail->Password = 'ics2608asdfghjkl';
+			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+			$mail->Port = '587';
 
-	$mail->MsgHTML($content); 
-	if(!$mail->Send()) {
-  		$alert = '<div class = "alert-success">
-				<span>Message sent! Thank you for contacting us.</span>
-				</div>';
-	} else {
-  		$alert = '<div class = "alert-error">
-				<span>'.$e->getMessage().'</span>
-				</div>';
+			$mail->setFrom('jemina.cayme.sy@gmail.com');
+			$mail->addAddress('jemina.cayme.sy@gmail.com');
+
+			$mail->isHTML(true);
+			$mail->Subject = 'New Form Submission (Contact Us)';
+			$mail->Body = "<h3>First Name: $fname <br /> 
+						Last Name: $lname <br />
+						Phone Number: $phoneNum <br /> 
+						Email: $email <br /> 
+						Package: $package <br /> 
+						Message: $message</h3>";
+
+			$mail->send();
+			$alert = '<div class = "alert-success">
+						<span>Message sent! Thank you for contacting us.</span>
+					</div>';
+		} catch (Exception $e){
+			$alert = '<div class = "alert-error">
+						<span>'.$e->getMessage().'</span>
+					</div>';
+		}
 	}
 ?>
